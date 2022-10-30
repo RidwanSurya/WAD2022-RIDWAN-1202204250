3ad27211533b92aa9ce001097a546d83dc8da098
RidwanSurya <ridwansuryaghani@gmail.com>
2022-10-30 19:16:25 +0700

    pindahan

---
+++ MODUL2 RIDWAN/BOOKING.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    </head>
+  
+  <body>
+    <section id="Home">
+      <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+        <div class="container-fluid">
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav" style="margin-left: auto ; margin-right: auto;">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <a class="nav-link" href="#">Booking</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </section>
+    </br>
+    <h1 align="center">WELCOME TO EAD RENT</h1>
+    <h5 align="center" style="color:grey ;">Find your best car and deal it, here</h5>
+  </br>
